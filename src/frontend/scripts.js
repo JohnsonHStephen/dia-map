c7502c0ff207b93ff4ae4f669dc33ded5ed2bbea
Stephen Johnson <johnsonh.stephen@gmail.com>
2025-03-11 21:25:13 -0400

    add: escape key to leave overlay

--- src/frontend/scripts.js
+++ src/frontend/scripts.js
@@ -143,6 +143,12 @@ closeModal.onclick = function() {
     modal.style.display = 'none';
 }
 
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        modal.style.display = 'none';
+    }
+});
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
